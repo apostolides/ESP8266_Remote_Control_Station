a69562064ae48148b2c84e4de5ee70bfbaae79ca
Thanos Apostolidis <53544333+apostolides@users.noreply.github.com>
2020-04-12 02:35:17 +0300

    Add files via upload

--- src/server/server.ino
+++ src/server/server.ino
@@ -6,9 +6,10 @@
 #include <IRsend.h>
 #include "ircodes.h"
 #include "tv_page.h"
+#include "index_page.h"
 
-const char *ssid = // ssid here
-const char *password = // password here
+const char *ssid = //
+const char *password = //
 const char *domain = "station";
 const int port = 80;
 const int ir_led_pin = 12;
@@ -21,10 +22,16 @@ ESP8266WebServer server(port);
 
 void rootHandler(){
   Serial.println("Requested root");
-	server.send(200, "text/html", tv_page);
+	server.send(200, "text/html", index_page);
 }
 
 // ----- Start of TV Controller ----- //
+
+void tvRemoteHandler(){
+  Serial.println("Requested tv remote main page");
+  server.send(200, "text/html", tv_page);
+}
+
 void tvPowerHandler(){
   Serial.println("Requested tv power");
   irsend.sendRaw(rawDataTVPower,sizeof(rawDataTVPower),38);
@@ -61,42 +68,71 @@ void tvChannelDownHandler(){
   server.send(200, "text/html", "Requested tv channel down"); 
 }
 
+void tvMute(){
+  Serial.println("Requested tv mute");
+  irsend.sendRaw(rawDataMute, sizeof(rawDataMute), 38);
+  server.send(200, "text/html", "Requested tv mute");
+}
+
+void tvButton0Handler(){
+  Serial.println("Requested channel 0");
+  irsend.sendRaw(tvrawData0,sizeof(tvrawData0),38);
+  server.send(200, "text/html", "Requested channel 0");
+}
+
 void tvButton1Handler(){
-  
+  Serial.println("Requested channel 1");
+  irsend.sendRaw(tvrawData1,sizeof(tvrawData1),38);
+  server.send(200, "text/html", "Requested channel 1");
 }
 
 void tvButton2Handler(){
-  
+  Serial.println("Requested channel 2");
+  irsend.sendRaw(tvrawData2,sizeof(tvrawData2),38);
+  server.send(200, "text/html", "Requested channel 2");
 }
 
 void tvButton3Handler(){
-  
+  Serial.println("Requested channel 3");
+  irsend.sendRaw(tvrawData3,sizeof(tvrawData3),38);
+  server.send(200, "text/html", "Requested channel 3");
 }
 
 void tvButton4Handler(){
-  
+  Serial.println("Requested channel 4");
+  irsend.sendRaw(tvrawData4,sizeof(tvrawData4),38);
+  server.send(200, "text/html", "Requested channel 4");
 }
 
 void tvButton5Handler(){
-  
+  Serial.println("Requested channel 5");
+  irsend.sendRaw(tvrawData5,sizeof(tvrawData5),38);
+  server.send(200, "text/html", "Requested channel 5");
 }
 
 void tvButton6Handler(){
-  
+  Serial.println("Requested channel 6");
+  irsend.sendRaw(tvrawData6,sizeof(tvrawData6),38);
+  server.send(200, "text/html", "Requested channel 6");
 }
 
 void tvButton7Handler(){
-  
+  Serial.println("Requested channel 7");
+  irsend.sendRaw(tvrawData7,sizeof(tvrawData7),38);
+  server.send(200, "text/html", "Requested channel 7");
 }
 
 void tvButton8Handler(){
-  
+  Serial.println("Requested channel 8");
+  irsend.sendRaw(tvrawData8,sizeof(tvrawData8),38);
+  server.send(200, "text/html", "Requested channel 8");
 }
 
 void tvButton9Handler(){
-  
+  Serial.println("Requested channel 9");
+  irsend.sendRaw(tvrawData9,sizeof(tvrawData9),38);
+  server.send(200, "text/html", "Requested channel 9");
 }
-
 // ----- End of TV Controller ----- //
 
 // ----- End of Handlers ----- //
@@ -117,13 +153,26 @@ void setup(){
 
   irsend.begin();
 
-  server.on("/",rootHandler);
+  server.on("/", rootHandler);
+  server.on("/tvRemote",tvRemoteHandler);
+  server.on("/tvMute",tvMuteHandler);
   server.on("/tvPower",tvPowerHandler);
   server.on("/tvVolumeUp",tvVolumeUpHandler);
   server.on("/tvVolumeDown",tvVolumeDownHandler);
   server.on("/tvChannelUp",tvChannelUpHandler);
   server.on("/tvChannelDown",tvChannelDownHandler);
- 
+  server.on("/tvMute", tvMute);
+  server.on("/tv0", tvButton0Handler);
+  server.on("/tv1", tvButton1Handler);
+  server.on("/tv2", tvButton2Handler);
+  server.on("/tv3", tvButton3Handler);
+  server.on("/tv4", tvButton4Handler);
+  server.on("/tv5", tvButton5Handler);
+  server.on("/tv6", tvButton6Handler);
+  server.on("/tv7", tvButton7Handler);
+  server.on("/tv8", tvButton8Handler);
+  server.on("/tv9", tvButton9Handler);
+  
   server.begin();
 
 }
